067006d32b9b1aa6113b99feee34564c99321060
Vivien Monneret <vmonneret30@gmail.com>
2020-02-09 16:59:13 +0100

    added detailed parser and cleaned
    saving not work here

---
+++ tools/detailed_parsers.js
@@ -0,0 +1,100 @@
+module.exports = {
+
+  // from the exrtracted stat string, return the value and modifier of a stat
+  // ex "21 (+5)" => { value: 21, mod: 5 }
+  parseStats: stat_string => {
+    str_split = stat_string.split("(");
+    value = parseInt(str_split[0]);
+    mod = parseInt(str_split[1].replace(")", "").replace("−", "-"));
+    return {
+      value,
+      mod
+    };
+  },
+
+  // from the extracted hp sting, return the hp value and roll parameters
+  // ex "135 (18d10 + 36)" => {value: 135, roll: { number: 18, dice: 10, mod: 36 }}
+  parseHP: hp_string => {
+    // cutting the string at the '(', ')', '+', and '−' /!\ different from '-'
+    str_split = hp_string.split(/[()d\+−]+/);
+
+    value = parseInt(str_split[0]);
+    number = parseInt(str_split[1]);
+    dice = parseInt(str_split[2]);
+    mod = 0;
+
+    // check if modifier and of what sign. 4 because last value is ''
+    if (str_split.length > 4) {
+      mod = parseInt(str_split[3]);
+      if (hp_string.includes("−")) mod = -mod;
+    }
+
+    hp = {
+      value,
+      roll: {
+        number,
+        dice,
+        mod
+      }
+    };
+    return hp;
+  },
+
+  // from the extracted challenge sting, return the cr value and xp
+  // ex "10 (5,900 XP)" => { value: 10, xp: 5900 }
+  parseChallenge: challenge_string => {
+    str_split = challenge_string.split(/[()XP]+/);
+
+    value = str_split[0];
+    xp = str_split[1];
+
+    // deal with '/' for cr is less than 1
+    if (value.includes("/")) {
+      value = 1.0 / parseFloat(value.split("/")[1]);
+    } else {
+      value = parseInt(value);
+    }
+
+    // deal with ',' for xp more than 1,000
+    if (xp.includes(",")) {
+      xp_split = xp.split(",");
+      xp = 1000 * parseInt(xp_split[0]) + parseInt(xp_split[1]);
+    } else {
+      xp = parseInt(xp);
+    }
+
+    challenge = {
+      value,
+      xp
+    };
+    return challenge;
+  },
+
+  // from the extracted saving sting, return the dict of savings
+  // ex "Con +6, Int +8, Wis +6" => { CON: 6, INT: 8, WIS: 6 }
+  parseSaving: saving_str => {
+    str_split = saving_str.split(/[,]+/);
+
+    saving = {};
+    for (stat of str_split) {
+      stat_split = stat.split("+");
+      saving[stat_split[0].toUpperCase()] = parseInt(stat_split[1]);
+    }
+
+    return saving;
+  },
+
+  // from the extracted skill sting, return the dict of skills
+  // ex "History +12, Perception +10" => { history: 12, perception: 10 }
+  parseSkills: skill_str => {
+    str_split = skill_str.split(/[,()]+/);
+    //TODO: finish this and check for special cases: ()
+    skills = {};
+    for (skill of str_split) {
+      skill_split = skill.split("+");
+      skills[stat_split[0].toLowerCase()] = parseInt(stat_split[1]);
+    }
+
+    return skills;
+  },
+}
--- tools/parse_monster_stats_json.js
+++ tools/parse_monster_stats_json.js
@@ -1,45 +1,49 @@
-const fs = require('fs');
+const fs = require("fs");
 const Creature = require("../db/models/Monster");
-const connectDB = require('../db/db');
+const connectDB = require("../db/db");
+const { parseStats, parseHP, parseChallenge, parseSaving } = require("./detailed_parsers")
+
 
 getAttribute = (content_array, header) => {
-  for(attribute of content_array){
-    
-    if(typeof(attribute) === "string" && attribute.includes(header)) {
-      return attribute.split(header).join('');
+  for (attribute of content_array) {
+    if (typeof attribute === "string" && attribute.includes(header)) {
+      return attribute.split(header).join("");
     }
   }
   return null;
-}
+};
+
 
 fillContent = (monster_name, monster_content) => {
   // console.log(monster_name);
 
   // stats extraction
-  var stats ={};
-  for (let [key, value] of Object.entries( monster_content[4].table)) {
-    stats[key] = value[0];
+  var stats = {};
+  for (let [key, value] of Object.entries(monster_content[4].table)) {
+    stats[key] = parseStats(value[0]);
   }
 
   // attribures extraction
-  const required_attributes = {
-    senses: "**Senses** ",
-    languages: "**Languages** ",
-    challenge: "**Challenge** ",
-  };
-  const optional_attributes = {
-    skills: "**Skills** ",
-    saving: "**Saving Throws** ",
-    resistances: "**Damage Resistances** ",
-    immunities: "**Condition Immunities** ",
+  const attribute_infos = {
+    senses:       { header: "**Senses** ",                required: true,   parser: null },
+    languages:    { header: "**Languages** ",             required: true,   parser: null },
+    challenge:    { header: "**Challenge** ",             required: true,   parser: parseChallenge },
+    skills:       { header: "**Skills** ",                required: false,  parser: null },
+    saving:       { header: "**Saving Throws** ",         required: false,  parser: parseSaving },
+    resistances:  { header: "**Damage Resistances** ",    required: false,  parser: null },
+    immunities:   { header: "**Condition Immunities** ",  required: false,  parser: null }
   };
   var attributes = {};
-  for([key, header] of Object.entries(required_attributes)){
-    attributes[key] = getAttribute(monster_content, header);
-  }
-  for([key, header] of Object.entries(optional_attributes)){
-    body =  getAttribute(monster_content, header);
-    if (body !== null) attributes[key] = body;
+  for ([key, { header, required, parser }] of Object.entries(attribute_infos)) {
+    body = getAttribute(monster_content, header);
+    if (body !== null) {
+      attributes[key] = body
+      if (parser) {
+        attributes[key] = parser(attributes[key]);
+      }
+    } else if (required) {
+      error("missing argument")
+    }
   }
 
   // traits extraction
@@ -48,46 +52,58 @@ fillContent = (monster_name, monster_content) => {
   while (!monster_content[index++].includes("**Challenge**")) {}
   traits = [];
   // get all the lines before Actions
-  while (index < monster_content.length && !monster_content[index].includes("**Actions**")){
-    traits.push(monster_content[index])
+  while (
+    index < monster_content.length &&
+    !monster_content[index].includes("**Actions**")
+  ) {
+    traits.push(monster_content[index]);
     index++;
   }
 
   // actions extraction
   var index = 5;
-  while (index < monster_content.length && !monster_content[index++].includes("**Actions**")) {}
+  while (
+    index < monster_content.length &&
+    !monster_content[index++].includes("**Actions**")
+  ) {}
   actions = [];
   // get all the lines before Actions
-  while (index < monster_content.length){
+  while (index < monster_content.length) {
     line = monster_content[index];
     // break when entering another category
-    if (line.slice(0, 2) === "**" && line.slice(0, 3) !== "***") break; 
+    if (line.slice(0, 2) === "**" && line.slice(0, 3) !== "***") break;
     actions.push(line);
     index++;
   }
 
   // reactions extraction
   var index = 5;
-  while (index < monster_content.length && !monster_content[index++].includes("**Reactions**")) {}
+  while (
+    index < monster_content.length &&
+    !monster_content[index++].includes("**Reactions**")
+  ) {}
   reactions = [];
   // get all the lines before Actions
-  while (index < monster_content.length){
+  while (index < monster_content.length) {
     line = monster_content[index];
     // break when entering another category
-    if (line.slice(0, 2) === "**" && line.slice(0, 3) !== "***") break; 
+    if (line.slice(0, 2) === "**" && line.slice(0, 3) !== "***") break;
     reactions.push(line);
     index++;
   }
 
   // reactions extraction
   var index = 5;
-  while (index < monster_content.length && !monster_content[index++].includes("**Legendary Actions**")) {}
+  while (
+    index < monster_content.length &&
+    !monster_content[index++].includes("**Legendary Actions**")
+  ) {}
   legendaryAction = [];
   // get all the lines before Actions
-  while (index < monster_content.length){
+  while (index < monster_content.length) {
     line = monster_content[index];
     // break when entering another category
-    if (line.slice(0, 2) === "**" && line.slice(0, 3) !== "***") break; 
+    if (line.slice(0, 2) === "**" && line.slice(0, 3) !== "***") break;
     legendaryAction.push(line);
     index++;
   }
@@ -95,11 +111,11 @@ fillContent = (monster_name, monster_content) => {
   // todo: nodes last index
   monster = new Monster({
     name: monster_name,
-    meta: monster_content[0].split('*').join(''), // removes '*' around
+    meta: monster_content[0].split("*").join(""), // removes '*' around
     ac: getAttribute(monster_content, "**Armor Class** "),
-    hp: getAttribute(monster_content, "**Hit Points** "),
+    hp: parseHP(getAttribute(monster_content, "**Hit Points** ")),
     speed: getAttribute(monster_content, "**Speed** "),
-    stats, 
+    stats,
     attributes,
     description: {
       traits,
@@ -109,80 +125,79 @@ fillContent = (monster_name, monster_content) => {
     }
   });
 
+  return monster;
+};
 
-  return monster
-}
-
-const exceptions = [
-  "Half-Dragon Template",
-];
-getContent = (group, prefix="") => {
+const exceptions = ["Half-Dragon Template"];
+getContent = (group, prefix = "") => {
   let monster_list = {};
   for (let [monster_name, monster_content] of Object.entries(group)) {
-    if (exceptions.includes(monster_name)){
+    if (exceptions.includes(monster_name)) {
       continue;
     }
-    if ('content' in monster_content){
-      monster_list[prefix + monster_name] =  fillContent(monster_name, monster_content.content);
+    if ("content" in monster_content) {
+      monster_list[prefix + monster_name] = fillContent(
+        monster_name,
+        monster_content.content
+      );
     } else {
-      monster_list = {...monster_list, ...getContent(monster_content, monster_name + ': ')};
+      monster_list = {
+        ...monster_list,
+        ...getContent(monster_content, monster_name + ": ")
+      };
     }
   }
   return monster_list;
-}
+};
 
-listMonsters = (rawdata) =>{
+listMonsters = rawdata => {
   let monster_list = {};
   let monsters = JSON.parse(rawdata).Monsters;
   for (let [category, category_content] of Object.entries(monsters)) {
-    if(category.split(' ')[0] === "Monsters") {
-      monster_list = {...monster_list, ...getContent(category_content)};
-      
+    if (category.split(" ")[0] === "Monsters") {
+      monster_list = { ...monster_list, ...getContent(category_content) };
     }
   }
   // console.log(monster_list);
   // console.log(monster_list["Aboleth"].description.legendaryAction);
-  
-  return monster_list
-}
 
-linkImage = (rawdata, monster_list, ) => {
+  return monster_list;
+};
+
+linkImage = (rawdata, monster_list) => {
   let image_dict = JSON.parse(rawdata);
   for (let [monster_name, monster] of Object.entries(monster_list)) {
     name = monster.name;
     // console.log(name);
     image = image_dict[name];
-    if(image) {
+    if (image) {
       monster.image = image;
     } else {
       console.error("not found: " + name);
     }
     // TODO: find not found: Elf, Drow
     // not found: Succubus/Incubus
-    
   }
-}
+};
 
-
-extractMonsters = () =>{
+extractMonsters = () => {
   // console.log(fs.readdirSync('tools/json'));
-  let monsters_raw = fs.readFileSync('tools/json/11 monsters.json');
-  monster_list = listMonsters(monsters_raw); 
+  let monsters_raw = fs.readFileSync("tools/json/11 monsters.json");
+  monster_list = listMonsters(monsters_raw);
 
-  let img_raw = fs.readFileSync('tools/json/monster_img.json');
+  let img_raw = fs.readFileSync("tools/json/monster_img.json");
   linkImage(img_raw, monster_list);
 
   // console.log(monster_list);
-  
-  // saveMonsters(monster_list);
-}
 
-saveMonsters = async (monsters) => {
+  // saveMonsters(monster_list);
+};
+
+saveMonsters = async monsters => {
   await connectDB();
-  for(monster of Object.values(monster_list)) {
+  for (monster of Object.values(monster_list)) {
     await monster.save();
   }
-}
-
+};
 
 extractMonsters();
