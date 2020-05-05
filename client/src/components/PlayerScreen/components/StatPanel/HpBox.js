e6e70de095f7e08d5405c150fc2a38d6aed3f82d
Erwan Béguin <erwan.beguin@gmail.com>
2020-05-05 18:33:14 +0200

    grommetify hpbox

--- client/src/components/PlayerScreen/components/StatPanel/HpBox.js
+++ client/src/components/PlayerScreen/components/StatPanel/HpBox.js
@@ -1,88 +1,77 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Box } from "grommet";
+import "./HpBox.css";
+import HpManager from "../HpManager";
 
-import './HpBox.css'
-import HpManager from '../HpManager'
+//Hit points display managment
 
-//Hit points display managment 
+class HpBox extends Component {
+  constructor() {
+    super();
 
-class HpBox extends Component{
-    constructor(){
-        super()
+    this.state = {
+      hp: 30,
+      maxHp: 50,
 
-        this.state = {
-            
-            hp:30,
-            maxHp:50,
-           
-            userInput:0
-        }
+      userInput: 0,
+    };
 
-        this.userInput =this.userInput.bind(this)
-        this.addHP =this.addHP.bind(this)
-        this.remHP =this.remHP.bind(this)
+    this.userInput = this.userInput.bind(this);
+    this.addHP = this.addHP.bind(this);
+    this.remHP = this.remHP.bind(this);
+  }
+
+  addHP() {
+    var newHp = this.state.hp + parseInt(this.state.userInput, 10);
+    if (newHp > this.state.maxHp) {
+      this.setState({
+        hp: this.state.maxHp,
+      });
+    } else {
+      this.setState({
+        hp: newHp,
+      });
     }
+  }
 
-    addHP(){
-        var newHp=this.state.hp+parseInt(this.state.userInput,10)
-        if(newHp>this.state.maxHp){
-            this.setState({
-                hp:this.state.maxHp
-            })
-        }else{
-            this.setState({
-                hp:newHp
-            })
-        }
+  remHP() {
+    var newHp = this.state.hp - parseInt(this.state.userInput, 10);
+    if (newHp < 0) {
+      this.setState({
+        hp: 0,
+      });
+    } else {
+      this.setState({
+        hp: newHp,
+      });
     }
+  }
 
-    remHP(){
-        var newHp=this.state.hp-parseInt(this.state.userInput,10)
-        if(newHp<0){
-            this.setState({
-                hp:0
-            })
-        }else{
-            this.setState({
-                hp:newHp
-            })
-        }
-    }
+  userInput(e) {
+    this.setState({
+      userInput: e.target.value,
+    });
+  }
 
-    userInput(e){
-        this.setState({
-            userInput:e.target.value
-        })
-    }
+  render() {
+    return (
+      <Box className="Box">
+        <h4>Hit points</h4>
 
-    render(){
-        return(
+        <Box className="currentHp">
+          <HpManager />
+        </Box>
+        <hr />
+        <Box className="maxHp">{this.state.maxHp}</Box>
 
-            <div className="Box">
-
-                <h4>
-                    Hit points
-                </h4>
-
-                <div className="currentHp"><HpManager /></div>
-                <hr />
-                <div className="maxHp">{this.state.maxHp}</div>
-
-
-
-                <div className="modifyHp">
-                    <button onClick={this.remHP}>-</button>
-                    <input onChange={this.userInput}/>
-                    <button onClick={this.addHP}>+</button>   
-                </div>
-
-            </div>
-
-
-        )
-    }
-
-
-
+        <Box direction="row">
+          <button onClick={this.remHP}>-</button>
+          <input onChange={this.userInput} />
+          <button onClick={this.addHP}>+</button>
+        </Box>
+      </Box>
+    );
+  }
 }
 
-export default HpBox
+export default HpBox;
