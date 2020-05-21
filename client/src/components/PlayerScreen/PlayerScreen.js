ded8a112ff2f48d43a77a3562fadafb0831dfacd
Erwan Béguin <erwan.beguin@gmail.com>
2020-05-21 16:32:09 +0200

    Ajout Map + LorePanel

--- client/src/components/PlayerScreen/PlayerScreen.js
+++ client/src/components/PlayerScreen/PlayerScreen.js
@@ -1,13 +1,33 @@
 import React from "react";
-import { Box } from "grommet";
+import { Box, DropButton } from "grommet";
 import StatPanel from "./components/StatPanel/StatPanel";
 import ActionPanel from "./components/ActionPanel/ActionPanel";
+import Map from "../Map/Map";
+import LorePanel from "./components/LorePanel/LorePanel";
 
 const PlayerScreen = () => {
   return (
     <Box>
       <StatPanel />
-      <ActionPanel />
+      <Box direction="row" gap="small">
+        <ActionPanel />
+        <Box height="small">
+          <DropButton
+            color="black"
+            dropAlign={{ top: "bottom" }}
+            label="Map"
+            dropContent={<Map />}
+          />
+        </Box>
+        <Box height="small">
+          <DropButton
+            color="black"
+            dropAlign={{ top: "bottom" }}
+            label="Lore"
+            dropContent={<LorePanel />}
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };
---
+++ client/src/components/PlayerScreen/components/LorePanel/LorePanel.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Text } from "grommet";
+
+const LorePanel = () => {
+  return (
+    <Box align="center">
+      <Text>Lore</Text>
+    </Box>
+  );
+};
+
+export default LorePanel;
