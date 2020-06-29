da68501727876021775071fc008cdd6516d1ab83
Quentin Rouquette <quentin.rouquette@gmail.com>
2020-06-29 17:29:38 +0200

    Fixed zoom bug

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -33,21 +33,22 @@ const Map = ({
                  user
              }) => {
     useEffect(() => {
-
-        // Load map image
         loadActiveImage();
+    }, []);
 
-        // Listen for map panning to move tokens accordingly
-        new MutationObserver(function(mutations) {
-            mutations.forEach(function(mutationRecord) {
-                tokensWrapper.current.style.transform = zppRef.current.contentRef.current.style.transform
-            });
-        }).observe(zppRef.current.contentRef.current, { attributes : true, attributeFilter : ['style'] });
-    })
-
-    // State variables
     const [imgSize, setImgSize] = React.useState({width: "0px", height: "0px"})
     const [scale, setScale] = React.useState(1)
+    const [transform, setTransform] = React.useState("initial")
+
+    const handleStop = (e, position, id) => {
+        const { x, y } = position;
+        updatePosition({
+            id: id,
+            posx: x,
+            posy: y,
+        });
+    };
+
     const draggableTokens = involved.map(i=> {
         return {
             position: {
@@ -58,102 +59,30 @@ const Map = ({
         }
     })
 
-    // DOM Refs
-    const imgRef = React.useRef(null)
-    const zppRef = React.useRef(null)
-    const tokensWrapper = React.useRef(null)
-
-
-    // Methods
-    /** Triggered when a token stops moving */
-    const handleStop = (e, position, id) => {
-        const { x, y } = position;
-        updatePosition({
-            id: id,
-            posx: x,
-            posy: y,
-        });
-    };
-
-    /** Triggered on image load or container size change */
-    const updateSize = (onLoad = false) => {
-
-        // Get element references
-        const img = imgRef.current
-        const wrapper = zppRef.current.wrapperRef.current
-        const state = zppRef.current.context.state
-
-        // Get current scales and positions
-        const currentScale = state.scale
-        const currentMinScale = state.options.minScale
-        const currentX = state.positionX
-        const currentY = state.positionY
-
-        // Calculate new min scale to fin map in screen
-        const newMinScale = Math.min(wrapper.offsetWidth / img.clientWidth, wrapper.offsetHeight / img.clientHeight)
-
-        // Calculate ratio of size change to keep a level of zoom relative to the one the user had before
-        const changeRatio = newMinScale / currentMinScale
-
-        // Calc new scale
-        const newScale = onLoad ? newMinScale : (currentScale * changeRatio)
-        setScale(newScale)
-
-        // Position the map properly manually since for some reason react-zoom-pan-pinch won't do it unles you click on it
-        const widthDiff = wrapper.offsetWidth - img.clientWidth * newScale
-        const heightDiff = wrapper.offsetHeight - img.clientHeight * newScale
-
-        const newX = widthDiff >= 0 || onLoad ? widthDiff/2 : Math.min(0, currentX * changeRatio)
-        const newY = heightDiff >=0 || onLoad ? heightDiff/2 : Math.min(0, currentY * changeRatio)
-
-        // Set values
-        zppRef.current.context.state.options.minScale = newMinScale
-        if (onLoad) {
-            // On first load a small timeout is needed for some reason
-            setTimeout(zppRef.current.context.dispatch.setTransform.bind(null, newX, newY, newScale, 0), 50)
-            setImgSize({width: img.clientWidth, height: img.clientHeight})
-        } else {
-            zppRef.current.context.dispatch.setTransform(newX, newY, newScale, 0)
-        }
-    }
-
-    return (
-        <Box fill={true} style={{position: "relative", overflow: "hidden"}}>
-            <TransformWrapper onZoomChange={(state)=>{setScale(state.scale)}} options={{centerContent: false}} wheel={{step: 200}} pan={{disabled: false}}>
-                <TransformComponent ref={zppRef}>
-                    <div>
-                        <img style={{objectFit: "none"}} onLoad={() => updateSize(true)} src={image} ref={imgRef} />
+    return <Box fill={true} style={{position: "relative", overflow: "hidden"}}>
+        <MovableMap image={image} onTransform={setTransform} onScaleChange={setScale} onImgLoad={setImgSize}/>
+        <div
+            style={{transformOrigin: "0% 0%", position: "absolute", ...imgSize, pointerEvents: "none", transform: transform}}
+        >
+            {draggableTokens.map((token) => (
+                <Draggable
+                    scale={scale}
+                    onStop={(e, position) => {
+                        token.position.x = position.x
+                        token.position.y = position.y
+                        handleStop(e, position, token.involved.creature._id)
+                    }}
+                    position={token.position}
+                >
+                    <div style={{pointerEvents: "all", display: "inline-block", position: "absolute"}}>
+                        <CreatureToken image={token.involved.creature.avatar} size={25} />
                     </div>
-                </TransformComponent>
-                <ReactResizeDetector handleWidth handleHeight onResize={() => {
-                    updateSize()
-                }} />
-            </TransformWrapper>
-            <div
-                ref={tokensWrapper}
-                style={{transformOrigin: "0% 0%", position: "absolute", ...imgSize, pointerEvents: "none"}}
-            >
-                {draggableTokens.map((token, i) => (
-                    <Draggable
-                        scale={scale}
-                        onStop={(e, position) => {
-                            console.log(scale)
-                            token.position.x = position.x
-                            token.position.y = position.y
-                            handleStop(e, position, token.involved.creature._id)
-                        }}
-                        // TODO: possible sync issue for attributes of attrivutes ?
-                        position={token.position}
-                    >
-                        <div style={{pointerEvents: "all"}}>{/*This div is needed for Draggable*/}
-                            <CreatureToken className="map-token" image={token.involved.creature.avatar} size={25} /></div>
-                    </Draggable>
-                ))}
-            </div>
-        </Box>
-    );
-};
+                </Draggable>
+            ))}
+        </div>
+    </Box>
 
+};
 Map.propTypes = {
     image: PropTypes.string,
     // loadImage: PropTypes.func,
@@ -189,3 +118,88 @@ export default connect(
         updatePosition,
     } // connect actions for the component to modify store state
 )(Map);
+
+
+class MovableMap extends React.PureComponent {
+
+    constructor(props) {
+        super(props);
+        this.zppRef = React.createRef()
+        this.imgRef = React.createRef()
+        this.state = {
+            scale: 1
+        }
+    }
+
+/*    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log("state", nextState, "props", nextProps)
+        return true
+    }*/
+
+    componentDidMount() {
+        // Listen for map panning to move tokens accordingly
+        new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutationRecord) {
+                this.props.onTransform(this.zppRef.current.contentRef.current.style.transform)
+            }.bind(this));
+        }.bind(this)).observe(this.zppRef.current.contentRef.current, { attributes : true, attributeFilter : ['style'] });
+    }
+
+    render() {
+        console.log("rendering map")
+        // Methods
+
+        /** Triggered on image load or container size change */
+        const updateSize = (onLoad = false) => {
+
+            const img = this.imgRef.current
+            const wrapper = this.zppRef.current.wrapperRef.current
+            const state = this.zppRef.current.context.state
+
+            // Get current scales and positions
+            const currentScale = state.scale
+            const currentMinScale = state.options.minScale
+            const currentX = state.positionX
+            const currentY = state.positionY
+
+            // Calculate new min scale to fin map in screen
+            const newMinScale = Math.min(wrapper.offsetWidth / img.clientWidth, wrapper.offsetHeight / img.clientHeight)
+
+            // Calculate ratio of size change to keep a level of zoom relative to the one the user had before
+            const changeRatio = newMinScale / currentMinScale
+
+            // Calc new scale
+            const newScale = onLoad ? newMinScale : (currentScale * changeRatio)
+            this.props.onScaleChange(newScale)
+
+            // Position the map properly manually since for some reason react-zoom-pan-pinch won't do it unles you click on it
+            const widthDiff = wrapper.offsetWidth - img.clientWidth * newScale
+            const heightDiff = wrapper.offsetHeight - img.clientHeight * newScale
+
+            const newX = widthDiff >= 0 || onLoad ? widthDiff/2 : Math.min(0, currentX * changeRatio)
+            const newY = heightDiff >=0 || onLoad ? heightDiff/2 : Math.min(0, currentY * changeRatio)
+
+            // Set values
+            this.zppRef.current.context.state.options.minScale = newMinScale
+            if (onLoad) {
+                // On first load a small timeout is needed for some reason
+                setTimeout(this.zppRef.current.context.dispatch.setTransform.bind(null, newX, newY, newScale, 0), 50)
+                this.props.onImgLoad({width: img.clientWidth, height: img.clientHeight})
+            } else {
+                this.zppRef.current.context.dispatch.setTransform(newX, newY, newScale, 0)
+            }
+        }
+
+        return (
+            <TransformWrapper onZoomChange={(state)=>{this.props.onScaleChange(state.scale)}} options={{centerContent: false}} wheel={{step: 200}} pan={{disabled: false}}>
+                <TransformComponent ref={this.zppRef}>
+                    <div>
+                        <img style={{objectFit: "none"}} onLoad={() => updateSize(true)} src={this.props.image} ref={this.imgRef} />
+                    </div>
+                </TransformComponent>
+                <ReactResizeDetector handleWidth handleHeight onResize={() => {
+                    updateSize()
+                }} />
+            </TransformWrapper>
+        );    }
+}
